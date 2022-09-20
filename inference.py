9a288f0238a8b0ade32e314a6842368ce6439baa
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-09-21 00:05:21 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,20 +1,24 @@
-from argparse import ArgumentParser
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
+
+from argparse import ArgumentParser
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('--image', type=str, required=True, help='test image path')
     parser.add_argument('--config', type = str, required=True, help = 'config file')
     parser.add_argument('--checkpoint', type = str, required=True, help = 'checkpoint file')
+    parser.add_argument('--threshhold', type = float, default=0.7, help = 'threshhold')
+    
     args = parser.parse_args()
 
     img_path = args.image
     checkpoint = args.checkpoint
     config = args.config
+    threshhold = args.threshhold
 
     model = init_detector(config, checkpoint, device = 'cuda')
     result = inference_detector(model, img_path)
-    show_result_pyplot(model, img_path, result, score_thr = 0.7)
+    show_result_pyplot(model, img_path, result, score_thr = threshhold)
 
 if __name__ == '__main__':
     main()
