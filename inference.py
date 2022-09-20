91a36ca93db19f3e2a7eabb69e4e1272e70a8217
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-09-21 00:02:41 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,5 @@
-from mmdet.apis import inference_detector, init_detector, show_result_pyplot
-
 from argparse import ArgumentParser
+from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 
 def main():
     parser = ArgumentParser()
