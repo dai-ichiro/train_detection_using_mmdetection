4dc10308d1efb11427ec2ff1a7429f5493204eb2
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-09-21 00:02:18 +0900

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,21 @@
+from mmdet.apis import inference_detector, init_detector, show_result_pyplot
+
+from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--image', type=str, required=True, help='test image path')
+    parser.add_argument('--config', type = str, required=True, help = 'config file')
+    parser.add_argument('--checkpoint', type = str, required=True, help = 'checkpoint file')
+    args = parser.parse_args()
+
+    img_path = args.image
+    checkpoint = args.checkpoint
+    config = args.config
+
+    model = init_detector(config, checkpoint, device = 'cuda')
+    result = inference_detector(model, img_path)
+    show_result_pyplot(model, img_path, result, score_thr = 0.7)
+
+if __name__ == '__main__':
+    main()
