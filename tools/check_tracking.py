0eb9dc58ff7089051ec86d8bda118497e5e06e58
dai <d.a.i-ichiro@outlook.jp>
2022-09-27 10:37:18 +0900

    update check_tracking.py

--- tools/check_tracking.py
+++ tools/check_tracking.py
@@ -1,6 +1,7 @@
 import os
 import os.path as osp
-import tempfile
+import torch
+from mim.commands.download import download
 from argparse import ArgumentParser
 
 import cv2
@@ -11,59 +12,24 @@ from mmtrack.apis import inference_sot, init_model
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--config', type=str, default='models/siamese_rpn_r50_20e_lasot.py', help='Config file')
     parser.add_argument('--input', help='input video file')
-    parser.add_argument('--output', help='output video file (mp4 format)')
-    parser.add_argument('--checkpoint', type=str, default='models/siamese_rpn_r50_20e_lasot_20220420_181845-dd0f151e.pth', help='Checkpoint file')
-    parser.add_argument(
-        '--device', default='cuda:0', help='Device used for inference')
-    parser.add_argument(
-        '--show',
-        action='store_true',
-        default=False,
-        help='whether to show visualizations.')
     parser.add_argument(
         '--color', default=(0, 255, 0), help='Color of tracked bbox lines.')
     parser.add_argument(
         '--thickness', default=3, type=int, help='Thickness of bbox lines.')
-    parser.add_argument('--fps', type=int, help='FPS of the output video')
-    parser.add_argument('--gt_bbox_file', help='The path of gt_bbox file')
     args = parser.parse_args()
 
-    # load images
-    if osp.isdir(args.input):
-        imgs = sorted(
-            filter(lambda x: x.endswith(('.jpg', '.png', '.jpeg')),
-                   os.listdir(args.input)),
-            key=lambda x: int(x.split('.')[0]))
-        IN_VIDEO = False
-    else:
-        imgs = mmcv.VideoReader(args.input)
-        IN_VIDEO = True
+    # load model
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    os.makedirs('models', exist_ok=True)
+    checkpoint_name = 'siamese_rpn_r50_20e_lasot'
+    checkpoint = download(package='mmtrack', configs=[checkpoint_name], dest_root="models")[0]
+    model = init_model(os.path.join('models', checkpoint_name + '.py'), os.path.join('models', checkpoint), device=device)
+    
+    # load videos
+    imgs = mmcv.VideoReader(args.input)
 
-    OUT_VIDEO = False
-    # define output
-    if args.output is not None:
-        if args.output.endswith('.mp4'):
-            OUT_VIDEO = True
-            out_dir = tempfile.TemporaryDirectory()
-            out_path = out_dir.name
-            _out = args.output.rsplit(os.sep, 1)
-            if len(_out) > 1:
-                os.makedirs(_out[0], exist_ok=True)
-        else:
-            out_path = args.output
-            os.makedirs(out_path, exist_ok=True)
-    fps = args.fps
-    if args.show or OUT_VIDEO:
-        if fps is None and IN_VIDEO:
-            fps = imgs.fps
-        if not fps:
-            raise ValueError('Please set the FPS for the output video.')
-        fps = int(fps)
-
-    # build the model from a config file and a checkpoint file
-    model = init_model(args.config, args.checkpoint, device=args.device)
+    fps = int(imgs.fps)
 
     prog_bar = mmcv.ProgressBar(len(imgs))
     # test and show/save the images
@@ -72,39 +38,22 @@ def main():
             img_path = osp.join(args.input, img)
             img = mmcv.imread(img_path)
         if i == 0:
-            if args.gt_bbox_file is not None:
-                bboxes = mmcv.list_from_file(args.gt_bbox_file)
-                init_bbox = list(map(float, bboxes[0].split(',')))
-            else:
-                init_bbox = list(cv2.selectROI(args.input, img, False, False))
+            init_bbox = list(cv2.selectROI(args.input, img, False, False))
 
             # convert (x1, y1, w, h) to (x1, y1, x2, y2)
             init_bbox[2] += init_bbox[0]
             init_bbox[3] += init_bbox[1]
 
         result = inference_sot(model, img, init_bbox, frame_id=i)
-        if args.output is not None:
-            if IN_VIDEO or OUT_VIDEO:
-                out_file = osp.join(out_path, f'{i:06d}.jpg')
-            else:
-                out_file = osp.join(out_path, img_path.rsplit(os.sep, 1)[-1])
-        else:
-            out_file = None
+        
         model.show_result(
             img,
             result,
-            show=args.show,
+            show = True,
             wait_time=int(1000. / fps) if fps else 0,
-            out_file=out_file,
+            out_file = None,
             thickness=args.thickness)
         prog_bar.update()
-
-    if args.output and OUT_VIDEO:
-        print(
-            f'\nmaking the output video at {args.output} with a FPS of {fps}')
-        mmcv.frames2video(out_path, args.output, fps=fps, fourcc='mp4v')
-        out_dir.cleanup()
-
 
 if __name__ == '__main__':
     main()
